4c7c51034ea57e9ad67db8aac4094c5f4d66e0b9
Rodrigo Argumedo <rodrigo.argumedo@icloud.com>
2015-05-27 15:00:13 -0600

    Removed Facebook stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User
   # :confirmable, :lockable, :timeoutable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable, :omniauth_providers => [:facebook]
+         :omniauthable
 
   ## Database authenticatable
   field :email, type: String, default: ""
@@ -42,19 +42,4 @@ class User
   field :name, :type => String
   field :username, :type => String
   field :nickname, :type => String
-  
-  def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.id).first_or_create do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
-    end
-  end
-  
-  def self.new_with_session(params, session)
-    super.tap do |user|
-      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-        user.email = data["email"] if user.email.blank?
-      end
-    end
-  end
 end
