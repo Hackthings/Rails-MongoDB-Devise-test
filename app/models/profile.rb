41827d301aaf32414c29cc3a8e0a019303f8eab9
Rodrigo Argumedo <rodrigo.argumedo@icloud.com>
2015-05-27 09:24:53 -0600

    Removed redundant Profile model

--- app/models/profile.rb
+++
@@ -1,9 +0,0 @@
-class Profile
-  include Mongoid::Document
-  # Basic info
-  field :first_name, type: String
-  field :last_name, type: String
-  field :age, type: Integer
-  field :dob, type: Time
-  field :bio, type: String
-end
