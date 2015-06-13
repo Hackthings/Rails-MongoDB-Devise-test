b8e64907fb7cb8be073b15c5fd171a1e1ae00d75
Rodrigo Argumedo <rodrigo.argumedo@icloud.com>
2015-06-13 10:39:05 -0600

    Fix markdown initializers

--- config/initializers/markdown_handler.rb
+++ config/initializers/markdown_handler.rb
@@ -1,6 +1,6 @@
 module MarkdownHandler
     def self.erb
-        @erb ||= ActionView::Template.registered_template(:erb)
+        @erb ||= ActionView::Template.registered_template_handler(:erb)
     end
     
     def self.call(template)
@@ -8,3 +8,6 @@ module MarkdownHandler
         "Kramdown::Document.new(begin;#{compiled_source};end, auto_ids: false).to_html.html_safe"
     end
 end
+
+ActionView::Template.register_template_handler :md, MarkdownHandler
+ActionView::Template.register_template_handler :markdown, MarkdownHandler
